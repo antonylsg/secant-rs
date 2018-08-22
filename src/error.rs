2f4b26fde77abb18b7a2c16d4b26af3acd0327e3
Antony Lesage <lesage.antony@gmail.com>
2018-08-22 12:54:38 +0200

    Simplify error handling

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,3 @@
-use std::error;
 use std::fmt;
 
 #[derive(Debug)]
@@ -10,20 +9,6 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Error::MaxIter(max) => write!(f, "Maximal iteration ({}) reached", max),
-        }
-    }
-}
-
-impl error::Error for Error {
-    fn description(&self) -> &str {
-        match *self {
-            Error::MaxIter(_) => "maximal iteration",
-        }
-    }
-
-    fn cause(&self) -> Option<&error::Error> {
-        match *self {
-            Error::MaxIter(_) => None,
         }
     }
 }
