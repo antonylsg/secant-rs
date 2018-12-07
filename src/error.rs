8d7489931a73d02e61af249bf71f1e53119f90dd
Antony Lesage <lesage.antony@gmail.com>
2018-12-07 09:12:01 +0100

    Bump to Rust 2018

--- src/error.rs
+++ src/error.rs
@@ -6,7 +6,7 @@ pub enum Error {
 }
 
 impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
             Error::MaxIter(max) => write!(f, "Maximal iteration ({}) reached", max),
         }
